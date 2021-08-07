7d7be0725d5320aada7df005be05cf404d969f39
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-08-07 21:54:44 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Lesson_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.pbx_Display = new System.Windows.Forms.PictureBox();
+            this.lbl_Model = new System.Windows.Forms.Label();
+            this.lbl_Vendor = new System.Windows.Forms.Label();
+            this.lbl_Year = new System.Windows.Forms.Label();
+            this.lbl_Engine = new System.Windows.Forms.Label();
+            this.btn_Back = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Next.Location = new System.Drawing.Point(197, 305);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(100, 38);
+            this.btn_Next.TabIndex = 1;
+            this.btn_Next.Text = "NEXT >>";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // pbx_Display
+            // 
+            this.pbx_Display.Location = new System.Drawing.Point(12, 12);
+            this.pbx_Display.Name = "pbx_Display";
+            this.pbx_Display.Size = new System.Drawing.Size(352, 265);
+            this.pbx_Display.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbx_Display.TabIndex = 2;
+            this.pbx_Display.TabStop = false;
+            // 
+            // lbl_Model
+            // 
+            this.lbl_Model.AutoSize = true;
+            this.lbl_Model.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Model.Location = new System.Drawing.Point(24, 25);
+            this.lbl_Model.Name = "lbl_Model";
+            this.lbl_Model.Size = new System.Drawing.Size(50, 16);
+            this.lbl_Model.TabIndex = 3;
+            this.lbl_Model.Text = "Model:";
+            // 
+            // lbl_Vendor
+            // 
+            this.lbl_Vendor.AutoSize = true;
+            this.lbl_Vendor.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Vendor.Location = new System.Drawing.Point(24, 79);
+            this.lbl_Vendor.Name = "lbl_Vendor";
+            this.lbl_Vendor.Size = new System.Drawing.Size(57, 16);
+            this.lbl_Vendor.TabIndex = 4;
+            this.lbl_Vendor.Text = "Vendor:";
+            // 
+            // lbl_Year
+            // 
+            this.lbl_Year.AutoSize = true;
+            this.lbl_Year.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Year.Location = new System.Drawing.Point(24, 137);
+            this.lbl_Year.Name = "lbl_Year";
+            this.lbl_Year.Size = new System.Drawing.Size(41, 16);
+            this.lbl_Year.TabIndex = 5;
+            this.lbl_Year.Text = "Year:";
+            // 
+            // lbl_Engine
+            // 
+            this.lbl_Engine.AutoSize = true;
+            this.lbl_Engine.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Engine.Location = new System.Drawing.Point(24, 197);
+            this.lbl_Engine.Name = "lbl_Engine";
+            this.lbl_Engine.Size = new System.Drawing.Size(55, 16);
+            this.lbl_Engine.TabIndex = 6;
+            this.lbl_Engine.Text = "Engine:";
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.Font = new System.Drawing.Font("Britannic Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Back.Location = new System.Drawing.Point(81, 305);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(100, 38);
+            this.btn_Back.TabIndex = 7;
+            this.btn_Back.Text = "<< BACK";
+            this.btn_Back.UseVisualStyleBackColor = true;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 369);
+            this.Controls.Add(this.btn_Back);
+            this.Controls.Add(this.lbl_Engine);
+            this.Controls.Add(this.lbl_Year);
+            this.Controls.Add(this.lbl_Vendor);
+            this.Controls.Add(this.lbl_Model);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.pbx_Display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.PictureBox pbx_Display;
+        private System.Windows.Forms.Label lbl_Model;
+        private System.Windows.Forms.Label lbl_Vendor;
+        private System.Windows.Forms.Label lbl_Year;
+        private System.Windows.Forms.Label lbl_Engine;
+        private System.Windows.Forms.Button btn_Back;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cars = new List<Car>
+            {
+                new Car
+                {
+                     Model = "BMW Alpina",
+                     Vendor = "Germany",
+                     Engine = 3.2M,
+                     Year = 2011
+                },
+                new Car
+                {
+                     Model = "Mercedes Mclaren",
+                     Vendor = "Germany",
+                     Engine = 6.1M,
+                     Year = 2009
+                },
+                new Car
+                {
+                     Model = "KIA Rio X-Line",
+                     Vendor = "Korean",
+                     Engine = 1.6M,
+                     Year = 2021
+                },
+                new Car
+                {
+                     Model = "Ferrary Pista",
+                     Vendor = "Italy",
+                     Engine = 4.4M,
+                     Year = 2014
+                },
+                new Car
+                {
+                     Model = "Lamborghini Murcielago",
+                     Vendor = "Italy",
+                     Engine = 6.5M,
+                     Year = 2018
+                }
+            };
+            pictures = new List<PictureBox>
+            {
+                new PictureBox
+                {
+                    Image = Properties.Resources.BMW,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Size = new Size(352, 265),
+                },
+                new PictureBox
+                {
+                    Image = Properties.Resources.Mercedes,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Size = new Size(352, 265),
+                },
+                new PictureBox
+                {
+                    Image = Properties.Resources.Kia,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Size = new Size(352, 265),
+                },
+                new PictureBox
+                {
+                    Image = Properties.Resources.Ferrari,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Size = new Size(352, 265),
+                },
+                new PictureBox
+                {
+                    Image = Properties.Resources.Lamba,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Size = new Size(352, 265),
+                },
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbx_Display.Image = pictures[Next].Image;
+            lbl_Model.Text = $"Model: {cars[Next].Model}";
+            lbl_Vendor.Text = $"Vendor: {cars[Next].Vendor}";
+            lbl_Year.Text = $"Year: {cars[Next].Year}";
+            lbl_Engine.Text = $"Engine: {cars[Next].Engine}";
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            if (Next < cars.Count - 1) Next++;
+            Form1_Load(this, e);
+        }
+
+        List<Car> cars;
+        List<PictureBox> pictures;
+        public short Next { get; set; } = 0;
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            if (Next - 1 >= 0) Next--;
+            Form1_Load(this, e);
+        }
+    }
+}
